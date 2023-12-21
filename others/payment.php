4d8811b02cb4d34a503c2099ccef477009ed7415
sakib <animehindized@gmail.com>
2023-12-21 11:05:52 +0530

    initail commit

---
+++ others/payment.php
@@ -0,0 +1,20 @@
+<?php
+include('../includes/conn.php');
+include("../functions/common_fun.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Page</title>
+</head>
+<h1>hdjgkdjo</h1>
+
+<body>
+
+</body>
+
+</html>
